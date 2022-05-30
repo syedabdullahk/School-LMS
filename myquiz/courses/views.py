d53a9c9315def2aa4db5122bae235e519f62d2e3
Syed Talha Ahmed Gardazi <atalha54@gmail.com>
2022-05-30 13:19:07 +0500

    fix serializers a bit

--- myquiz/courses/views.py
+++ myquiz/courses/views.py
@@ -12,7 +12,6 @@ from urllib import request
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_list_or_404
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
